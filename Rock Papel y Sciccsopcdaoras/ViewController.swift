7bfac1969bbca05062573d73e2a90c32e59496eb
Ethan <ethan.laudick24@d155.org>
2022-11-14 08:21:16 -0600

    Initial Commit

---
+++ Rock Papel y Sciccsopcdaoras/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rock Papel y Sciccsopcdaoras
+//
+//  Created by ETHAN LAUDICK on 11/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
